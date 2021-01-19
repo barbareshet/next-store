653a46b0a3e34a9a47a91332a5e80cac83851826
barbareshet <barbareshet@gmail.com>
2021-01-20 00:29:22 +0200

    initail setup

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+const MyApp = ({ Component, pageProps }) => {
+    return  (
+        <>
+            <Link href="/">
+                <a>Home</a>
+            </Link>
+            <Link href="/about">
+                <a>About</a>
+            </Link>
+            <Component {...pageProps} />
+            <footer>&copy; Next Store 2021 </footer>
+        </>
+    )
+}
+export default MyApp
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+const HomePage = () => {
+    return(
+        <div>
+            <h1>Welcome to Next Store</h1>
+        </div>
+    )
+}
+export default HomePage
